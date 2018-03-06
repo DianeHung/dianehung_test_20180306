34055859179ddb6f530dc493d233d44a6dfe9840
DianeHung <dianehung@yulonitsolutions.com>
2018-03-06 20:05:43 +0800

    py

---
+++ Untitled1.py
@@ -0,0 +1,112 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+joke = '''小明在睡覺被弟弟吵醒你好嗎？
+你在睡覺嗎？'''
+
+
+# In[2]:
+
+
+joke[0]
+
+
+# In[3]:
+
+
+joke[1]
+
+
+# In[7]:
+
+
+X=name.replace('j','T')
+
+
+# In[6]:
+
+
+name = 'jerry'
+'T' + name[1:]
+
+
+# In[8]:
+
+
+letters = 'asdfjkl;zxcvm,.qweruio'
+letters[:]
+
+
+# In[9]:
+
+
+letters[20:]
+
+
+# In[10]:
+
+
+letters[:21 :5]
+
+
+# In[11]:
+
+
+letters[19::4]
+
+
+# In[12]:
+
+
+str(98.6)
+
+
+# In[13]:
+
+
+todos = 'get gloves, get mask, give cat vitamins, call ambulace'
+todos.split(',')
+['get gloves','get mask','give cat vitamins','call ambulance']
+
+
+# In[14]:
+
+
+str = "this2009"
+
+
+# In[15]:
+
+
+str.isalnum()
+
+
+# In[16]:
+
+
+import requests
+respone = requests.get("http://www.novelscape.net/wxxs/j/jingyong.ffwz/001.htm")
+response.encoding = "big5"
+book1 = response.text
+
+
+# In[17]:
+
+
+import requests
+respone = requests.get("http://www.novelscape.net/sort/sort12/1.htm")
+response.encoding = "big5"
+book1 = response.text
+
+
+# In[ ]:
+
+
+import requests
+respone = requests.get("http://www.novelscape.net/sort/sort12/1.htm")
+response.encoding = "big5"
+book1 = response.text
+
